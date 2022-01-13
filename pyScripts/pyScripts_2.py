ed15d795691b8b76c20b1e73be2a067a1aa962e9
Vasu Pastagia <202112003@daiict.ac.in>
2022-01-13 17:07:04 +0530

    Pyscripts added

--- pyScripts/pyScripts_2.py
+++ pyScripts/pyScripts_2.py
@@ -1,8 +1,15 @@
-from openpyxl import Workbook
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.brainyquote.com/topics/motivational-quotes"
+r = requests.get(url)
+# print(r.content)
 
-wb = Workbook()
-# creating excel sheet
-ws1 = wb.create_sheet("MySheet")
-# changing sheet name
-ws1.title = "MySheet-1"
-print(wb.sheetnames)
+soap = BeautifulSoup(r.content, 'html5lib')
+# table = soap.find('div', attrs={'id': 'quotesList'})
+# print(table.prettify())
+
+# now im creating one html file which will contain all html code of given site link
+f = open("motivational.html", "w")
+f.write(soap.prettify())
+f.close()
+
